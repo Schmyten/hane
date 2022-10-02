bc5e790f402bab3a610f49d9bfc5a89db8c5fc87
Mathias Adam Møller <adam.and.math@gmail.com>
2022-10-02 14:34:43 +0200

    Primitive term printing

---
+++ hane-syntax/src/print.rs
@@ -0,0 +1,82 @@
+use std::fmt::{self, Write};
+
+type Term = hane_kernel::term::Term<String>;
+
+fn fresh(x: &String, names: &[String]) -> String {
+    if !names.contains(x) {
+        return x.to_owned();
+    }
+
+    let x = x.trim_end_matches(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
+    for n in 0usize.. {
+        let fresh = format!("{x}{n}");
+        if !names.contains(&fresh) {
+            return fresh;
+        }
+    }
+    unreachable!()
+}
+
+pub fn write_term(buf: &mut impl Write, term: &Term, names: &mut Vec<String>, level: usize) -> fmt::Result {
+    match term {
+        Term::Prop => write!(buf, "Prop"),
+        Term::Var(n) =>
+            if *n >= names.len() { write!(buf, "?:{}", n - names.len()) }
+            else { write!(buf, "{}", names[names.len() - n - 1]) },
+        Term::App(f, v) => {
+            if level < 10 { write!(buf, "(")?; }
+            write_term(buf, f, names, 10)?;
+            write!(buf, " ")?;
+            write_term(buf, v, names, 9)?;
+            if level < 10 { write!(buf, ")")?; }
+            Ok(())
+        },
+        Term::Product(x, x_tp, t) => {
+            let x = fresh(x, names);
+            if level < 200 { write!(buf, "(")?; }
+            write!(buf, "forall {x} : ")?;
+            write_term(buf, x_tp, names, 200)?;
+            write!(buf, ", ")?;
+            names.push(x);
+            let r = write_term(buf, t, names, 200);
+            names.pop();
+            r?;
+            if level < 200 { write!(buf, ")")?; }
+            Ok(())
+        },
+        Term::Abstract(x, x_tp, t) => {
+            let x = fresh(x, names);
+            if level < 200 { write!(buf, "(")?; }
+            write!(buf, "func {x} : ")?;
+            write_term(buf, x_tp, names, 200)?;
+            write!(buf, " => ")?;
+            names.push(x);
+            let r = write_term(buf, t, names, 200);
+            names.pop();
+            r?;
+            if level < 200 { write!(buf, ")")?; }
+            Ok(())
+        },
+        Term::Bind(x, x_tp, x_val, t) => {
+            let x = fresh(x, names);
+            if level < 200 { write!(buf, "(")?; }
+            write!(buf, "let {x} : ")?;
+            write_term(buf, x_tp, names, 200)?;
+            write!(buf, " := ")?;
+            write_term(buf, x_val, names, 200)?;
+            write!(buf, " in ")?;
+            names.push(x);
+            let r = write_term(buf, t, names, 200);
+            names.pop();
+            r?;
+            if level < 200 { write!(buf, ")")?; }
+            Ok(())
+        },
+    }
+}
+
+pub fn print_term(term: &Term, names: &mut Vec<String>, level: usize) -> String {
+    let mut buf = String::new();
+    write_term(&mut buf, term, names, level).unwrap();
+    buf
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use hane_kernel::term::Term;
+use hane_syntax::print::print_term;
+
 fn main() {
-    println!("Hello, world!");
+    let imply = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
+        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
+            Term::Product("p".to_owned(), Box::new(Term::Var(1)), Box::new(Term::Var(1)))
+        ))
+    ));
+    println!("let imply := {}", print_term(&imply, &mut Vec::new(), 200));
+    let mut names = vec!["imply".to_owned()];
+    let and = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
+        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
+            Term::Product("R".to_owned(), Box::new(Term::Prop), Box::new(
+                Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(
+                    Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(Term::Var(2)))), Box::new(
+                        Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(Term::Var(1)))), Box::new(
+                            Term::Var(0)
+                        ))
+                    ))
+                ))), Box::new(Term::Var(0)))
+            ))
+        ))
+    ));
+    println!("let and := {}", print_term(&and, &mut names, 200));
+    let and = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
+        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
+            Term::Product("R".to_owned(), Box::new(Term::Prop), Box::new(
+                Term::Product("f".to_owned(), Box::new(
+                    Term::Product("p".to_owned(), Box::new(Term::Var(2)), Box::new(
+                        Term::Product("q".to_owned(), Box::new(Term::Var(2)), Box::new(Term::Var(2)))
+                    ))
+                ), Box::new(Term::Var(1)))
+            ))
+        ))
+    ));
+    println!("and := {}", print_term(&and, &mut Vec::new(), 200));
+    let or = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
+        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
+            Term::Product("R".to_owned(), Box::new(Term::Prop), Box::new(
+                Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(
+                    Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(Term::Var(2)))), Box::new(Term::Var(0)))
+                ))), Box::new(
+                    Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(
+                        Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(Term::Var(1)))), Box::new(Term::Var(0)))
+                    ))), Box::new(
+                        Term::Var(0)
+                    ))
+                ))
+            ))
+        ))
+    ));
+    println!("let or := {}", print_term(&or, &mut names, 200));
+    let or = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
+        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
+            Term::Product("R".to_owned(), Box::new(Term::Prop), Box::new(
+                Term::Product("p".to_owned(), Box::new(
+                    Term::Product("p".to_owned(), Box::new(Term::Var(2)), Box::new(Term::Var(1)))
+                ), Box::new(
+                    Term::Product("q".to_owned(), Box::new(
+                        Term::Product("q".to_owned(), Box::new(Term::Var(2)), Box::new(Term::Var(2)))
+                    ), Box::new(Term::Var(2)))
+                ))
+            ))
+        ))
+    ));
+    println!("or := {}", print_term(&or, &mut Vec::new(), 200));
 }
