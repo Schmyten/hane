a5ed25b52e55198b9496c35ac4c2f04e442707bb
Mathias Adam Møller <adam.and.math@gmail.com>
2022-10-03 15:24:01 +0200

    Define normalization of terms

--- hane-kernel/src/term.rs
+++ hane-kernel/src/term.rs
@@ -1,3 +1,6 @@
+use std::cmp::Ordering;
+
+#[derive(Clone)]
 pub enum Term<B> {
     Prop,
     Var(usize),
@@ -5,4 +8,113 @@ pub enum Term<B> {
     Product(B, Box<Term<B>>, Box<Term<B>>),
     Abstract(B, Box<Term<B>>, Box<Term<B>>),
     Bind(B, Box<Term<B>>, Box<Term<B>>, Box<Term<B>>),
+}
+
+impl<B: Clone> Term<B> {
+    //TODO: Find a more descriptive name
+    pub fn push(&self, amount: usize) -> Self {
+        self.push_inner(0, amount)
+    }
+    
+    fn push_inner(&self, cut: usize, amount: usize) -> Self {
+        match self {
+            Term::Prop => Term::Prop,
+            Term::Var(n) => Term::Var(if cut <= *n { *n + amount } else { *n }),
+            Term::App(f, v) => Term::App(Box::new(f.push_inner(cut, amount)), Box::new(v.push_inner(cut, amount))),
+            Term::Product(x, x_tp, t) => Term::Product(x.clone(), Box::new(x_tp.push_inner(cut, amount)), Box::new(t.push_inner(cut+1, amount))),
+            Term::Abstract(x, x_tp, t) => Term::Abstract(x.clone(), Box::new(x_tp.push_inner(cut, amount)), Box::new(t.push_inner(cut+1, amount))),
+            Term::Bind(x, x_tp, x_val, t) => Term::Bind(x.clone(), Box::new(x_tp.push_inner(cut, amount)), Box::new(x_val.push_inner(cut, amount)), Box::new(t.push_inner(cut+1, amount))),
+        }
+    }
+
+    pub fn subst(&self, mut f: impl FnMut(usize, usize) -> Self) -> Self {
+        self.subst_inner(0, &mut f)
+    }
+    
+    fn subst_inner(&self, push: usize, f: &mut impl FnMut(usize, usize) -> Self) -> Self {
+        match self {
+            Term::Prop => Term::Prop,
+            Term::Var(n) => f(*n, push),
+            Term::App(t, v) => Term::App(Box::new(t.subst_inner(push, f)), Box::new(v.subst_inner(push, f))),
+            Term::Product(x, x_tp, t) => Term::Product(x.clone(), Box::new(x_tp.subst_inner(push, f)), Box::new(t.subst_inner(push+1, f))),
+            Term::Abstract(x, x_tp, t) => Term::Abstract(x.clone(), Box::new(x_tp.subst_inner(push, f)), Box::new(t.subst_inner(push+1, f))),
+            Term::Bind(x, x_tp, x_val, t) => Term::Bind(x.clone(), Box::new(x_tp.subst_inner(push, f)), Box::new(x_val.subst_inner(push, f)), Box::new(t.subst_inner(push+1, f))),
+        }
+    }
+
+    pub fn try_subst<E>(&self, mut f: impl FnMut(usize, usize) -> Result<Self, E>) -> Result<Self, E> {
+        self.try_subst_inner(0, &mut f)
+    }
+    
+    fn try_subst_inner<E>(&self, push: usize, f: &mut impl FnMut(usize, usize) -> Result<Self, E>) -> Result<Self, E> {
+        Ok(match self {
+            Term::Prop => Term::Prop,
+            Term::Var(n) => f(*n, push)?,
+            Term::App(t, v) => Term::App(Box::new(t.try_subst_inner(push, f)?), Box::new(v.try_subst_inner(push, f)?)),
+            Term::Product(x, x_tp, t) => Term::Product(x.clone(), Box::new(x_tp.try_subst_inner(push, f)?), Box::new(t.try_subst_inner(push+1, f)?)),
+            Term::Abstract(x, x_tp, t) => Term::Abstract(x.clone(), Box::new(x_tp.try_subst_inner(push, f)?), Box::new(t.try_subst_inner(push+1, f)?)),
+            Term::Bind(x, x_tp, x_val, t) => Term::Bind(x.clone(), Box::new(x_tp.try_subst_inner(push, f)?), Box::new(x_val.try_subst_inner(push, f)?), Box::new(t.try_subst_inner(push+1, f)?)),
+        })
+    }
+
+    pub fn subst_single(&self, n: usize, val: &Self) -> Self {
+        self.subst(|x, push| {
+            match (n + push).cmp(&x) {
+                Ordering::Less => Term::Var(x - 1),
+                Ordering::Equal => val.push(push),
+                Ordering::Greater => Term::Var(x),
+            }
+        })
+    }
+
+    pub fn pop(&self, n: usize) -> Option<Self> {
+        self.try_subst(|x, push|
+            match (n + push).cmp(&x) {
+                Ordering::Less => Ok(Term::Var(x - 1)),
+                Ordering::Equal => Err(()),
+                Ordering::Greater => Ok(Term::Var(x)),
+            }
+        ).ok()
+    }
+
+    pub fn normalize(&mut self) {
+        loop {
+            match self {
+                Term::Prop => break,
+                Term::Var(_) => break,
+                Term::App(f, v) => {
+                    f.normalize();
+                    v.normalize();
+
+                    if let Term::Abstract(_, _, t) = &**f {
+                        *self = t.subst_single(0, v);
+                        continue;
+                    }
+                },
+                Term::Product(_, t1, t2) => {
+                    t1.normalize();
+                    t2.normalize();
+                },
+                Term::Abstract(_, t, b) => {
+                    t.normalize();
+                    b.normalize();
+
+                    if let Term::App(f, v) = &**b {
+                        if let Term::Var(0) = &**v {
+                            if let Some(t) = f.pop(0) {
+                                *self = t;
+                                continue;
+                            }
+                        }
+                    }
+                },
+                Term::Bind(_, _, val, t) => {
+                    val.normalize();
+                    *self = t.subst_single(0, val);
+                    continue;
+                },
+            }
+            break;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,49 @@
 use hane_kernel::term::Term;
 use hane_syntax::print::print_term;
 
+use Term::Prop;
+use Term::Var;
+
+fn app<B>(f: Term<B>, args: impl IntoIterator<Item = Term<B>>) -> Term<B> {
+    args.into_iter().fold(f, |f, v| Term::App(Box::new(f), Box::new(v)))
+}
+
+fn product<B, I: IntoIterator<Item = (B, Term<B>)>>(xs: I, t: Term<B>) -> Term<B> where I::IntoIter: DoubleEndedIterator {
+    xs.into_iter().rev().fold(t, |t, (x, x_tp)| Term::Product(x, Box::new(x_tp), Box::new(t)))
+}
+
+fn lamda<B, I: IntoIterator<Item = (B, Term<B>)>>(xs: I, t: Term<B>) -> Term<B> where I::IntoIter: DoubleEndedIterator {
+    xs.into_iter().rev().fold(t, |t, (x, x_tp)| Term::Abstract(x, Box::new(x_tp), Box::new(t)))
+}
+
+fn bind<B>(x: B, x_tp: Term<B>, x_val: Term<B>, t: Term<B>) -> Term<B> {
+    Term::Bind(x, Box::new(x_tp), Box::new(x_val), Box::new(t))
+}
+
 fn main() {
-    let imply = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
-        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
-            Term::Product("p".to_owned(), Box::new(Term::Var(1)), Box::new(Term::Var(1)))
-        ))
-    ));
+    let conn = product([("P".to_owned(), Prop), ("Q".to_owned(), Prop)], Prop);
+    let imply = lamda([("P".to_owned(), Prop), ("Q".to_owned(), Prop)],
+        product([("p".to_owned(), Var(1))], Var(1))
+    );
     println!("let imply := {}", print_term(&imply, &mut Vec::new(), 200));
     let mut names = vec!["imply".to_owned()];
-    let and = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
-        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
-            Term::Product("R".to_owned(), Box::new(Term::Prop), Box::new(
-                Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(
-                    Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(Term::Var(2)))), Box::new(
-                        Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(Term::Var(1)))), Box::new(
-                            Term::Var(0)
-                        ))
-                    ))
-                ))), Box::new(Term::Var(0)))
-            ))
-        ))
-    ));
+
+    let and = lamda([("P".to_owned(), Prop), ("Q".to_owned(), Prop)],
+        product([("R".to_owned(), Prop)],
+            app(Var(3), [app(Var(3), [Var(2), app(Var(3), [Var(1), Var(0)])]), Var(0)])
+        )
+    );
     println!("let and := {}", print_term(&and, &mut names, 200));
-    let and = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
-        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
-            Term::Product("R".to_owned(), Box::new(Term::Prop), Box::new(
-                Term::Product("f".to_owned(), Box::new(
-                    Term::Product("p".to_owned(), Box::new(Term::Var(2)), Box::new(
-                        Term::Product("q".to_owned(), Box::new(Term::Var(2)), Box::new(Term::Var(2)))
-                    ))
-                ), Box::new(Term::Var(1)))
-            ))
-        ))
-    ));
-    println!("and := {}", print_term(&and, &mut Vec::new(), 200));
-    let or = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
-        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
-            Term::Product("R".to_owned(), Box::new(Term::Prop), Box::new(
-                Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(
-                    Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(Term::Var(2)))), Box::new(Term::Var(0)))
-                ))), Box::new(
-                    Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(
-                        Term::App(Box::new(Term::App(Box::new(Term::Var(3)), Box::new(Term::Var(1)))), Box::new(Term::Var(0)))
-                    ))), Box::new(
-                        Term::Var(0)
-                    ))
-                ))
-            ))
-        ))
-    ));
+    let mut and = bind("imply".to_owned(), conn.clone(), imply.clone(), and);
+    and.normalize();
+    println!("{}", print_term(&and, &mut Vec::new(), 200));
+    let or = lamda([("P".to_owned(), Prop), ("Q".to_owned(), Prop)],
+        product([("R".to_owned(), Prop)],
+            app(Var(3), [app(Var(3), [app(Var(3), [Var(2), Var(0)]), app(Var(3), [app(Var(3), [Var(1), Var(0)]), Var(0)])]), Var(0)])
+        )
+    );
     println!("let or := {}", print_term(&or, &mut names, 200));
-    let or = Term::Abstract("P".to_owned(), Box::new(Term::Prop), Box::new(
-        Term::Abstract("Q".to_owned(), Box::new(Term::Prop), Box::new(
-            Term::Product("R".to_owned(), Box::new(Term::Prop), Box::new(
-                Term::Product("p".to_owned(), Box::new(
-                    Term::Product("p".to_owned(), Box::new(Term::Var(2)), Box::new(Term::Var(1)))
-                ), Box::new(
-                    Term::Product("q".to_owned(), Box::new(
-                        Term::Product("q".to_owned(), Box::new(Term::Var(2)), Box::new(Term::Var(2)))
-                    ), Box::new(Term::Var(2)))
-                ))
-            ))
-        ))
-    ));
-    println!("or := {}", print_term(&or, &mut Vec::new(), 200));
+    let mut or = bind("imply".to_owned(), conn.clone(), imply.clone(), or);
+    or.normalize();
+    println!("{}", print_term(&or, &mut Vec::new(), 200));
 }
